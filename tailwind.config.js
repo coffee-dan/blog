2af635078cbfe7c1ce156eb7401456a996d7e266
coffee-dan <coffee-dan@users.noreply.github.com>
2023-08-10 06:55:20 +0000

    deploy: 7713124402e5f2b74049800b27e96a775298c85f

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // all the places where tailwind is used
+    content: [
+      './_authors/*.md',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.md',
+      './*.html',
+    ],
+    theme: {
+      theme: {
+        extend: {},
+      },
+    },
+    plugins: []
+  }
