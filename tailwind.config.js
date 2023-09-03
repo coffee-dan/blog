867f5f67531ecbed1ee58a0d2012f9062b7d88c0
coffee-dan <coffee-dan@users.noreply.github.com>
2023-09-03 21:34:23 +0000

    deploy: 1d13104fa799abae5f7add549df52980922d6a6f

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       './*.md',
       './*.html',
     ],
+    darkMode: 'class',
     // https://tailwindcss.com/docs/theme
     theme: {
       fontFamily: {
@@ -17,6 +18,17 @@ module.exports = {
       extend: {
         gridTemplateColumns: {
           'left-rail': '150px 1fr',
+        },
+        // see: https://getbootstrap.com/docs/5.0/layout/z-index/
+        zIndex: {
+          'dropdown': 1000,
+          'sticky': 1020,
+          'fixed': 1030,
+          'modal-backdrop': 1040,
+          'offcanvas': 1050,
+          'modal': 1060,
+          'popover': 1070,
+          'tooltip': 1080,
         }
       }
     },
